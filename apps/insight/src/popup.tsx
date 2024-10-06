5a1fdf59af5ebb8cb3b740404e0da241536b6fb3
Bohdan <76702178+bohdan-mykhailenko@users.noreply.github.com>
2024-10-06 15:38:28 +0300

    feat: add more ui styles

--- apps/insight/src/popup.tsx
+++ apps/insight/src/popup.tsx
@@ -1,4 +1,6 @@
-import { Button } from "~components/ui/button"
+import clsx from "clsx";
+
+import { Button } from "~components/ui/button";
 import {
   Card,
   CardContent,
@@ -6,28 +8,52 @@ import {
   CardFooter,
   CardHeader,
   CardTitle
-} from "~components/ui/card"
+} from "~components/ui/card";
 
-import "~style.css"
+import "~style.css";
+
+import { Connection } from "~types";
 
 function IndexPopup() {
+  const { getMessage } = chrome.i18n;
+
+  // todo: replace with real data
+  const getBalance = () => 100;
+  const getStatus = () => Connection.CONNECTED;
+
   return (
     <Card className="w-72">
-      <CardHeader className="bg-accent h-32 rounded-b-2xl">
-        <CardTitle>Card Title 00000000</CardTitle>
-        <CardDescription>Card Description</CardDescription>
-        <Button className="text-accent2" variant="outline">
-          Click Me
-        </Button>
-      </CardHeader>
-      <CardContent className="text-accent2">
-        <p className="text-accent2"> Card Content</p>
+      <CardHeader>HEADER</CardHeader>
+
+      <CardContent className="p-2.5 bg-accent h-32 rounded-b-2xl">
+        <div className="flex flex-row justify-between items-center mb-3">
+          <Button variant="default">{getMessage("connectWallet")}</Button>
+
+          <Button variant="default">{getMessage("depositFunds")}</Button>
+        </div>
+
+        <div className="flex flex-row mb-2 items-center h-8 bg-accent-foreground rounded">
+          <p className="px-3 text-primary-foreground font-medium text-base">
+            {`${getMessage("balance")}: ${getBalance()}`}
+          </p>
+        </div>
+
+        <p className="px-3 text-primary-foreground font-medium text-base">
+          {`${getMessage("status")}: `}
+          <span
+            className={
+              getStatus() === Connection.CONNECTED
+                ? "text-green-500"
+                : getStatus() === Connection.CONNECTING
+                  ? "text-yellow-500"
+                  : "text-red-500"
+            }>
+            {getStatus()}
+          </span>
+        </p>
       </CardContent>
-      <CardFooter>
-        <p>Card Footer</p>
-      </CardFooter>
     </Card>
-  )
+  );
 }
 
-export default IndexPopup
+export default IndexPopup;
---
+++ apps/insight/src/types.ts
@@ -0,0 +1,5 @@
+export enum Connection {
+  CONNECTED = "connected",
+  CONNECTING = "connecting",
+  DISCONNECTED = "disconnected"
+}
