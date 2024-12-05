0020a12658ab6af4bb162b68b882745c86b7d1d4
Bohdan <76702178+bohdan-mykhailenko@users.noreply.github.com>
2024-12-05 21:56:09 +0200

    chore: add base CSUI

--- apps/plasmo_new/src/content.tsx
+++ apps/plasmo_new/src/content.tsx
@@ -1,22 +1,34 @@
-import cssText from "data-text:~global.css";
-import type { PlasmoCSConfig } from "plasmo";
+// import cssText from "data-text:~global.css";
+// import type { PlasmoCSConfig } from "plasmo";
 
-export const config: PlasmoCSConfig = {
-  matches: ["<all_urls>"],
+// export const config: PlasmoCSConfig = {
+//   matches: ["<all_urls>"],
+//   all_frames: true,
+// };
+
+// export const getStyle = () => {
+//   const style = document.createElement("style");
+
+//   style.textContent = cssText;
+
+//   return style;
+// };
+
+// const PlasmoOverlay = () => {
+//   return (
+//     <div className="plasmo-z-50 plasmo-flex plasmo-fixed plasmo-top-32 plasmo-right-8"></div>
+//   );
+// };
+
+// console.log(
+//   "You may find that having is not so pleasing a thing as wanting. This is not logical, but it is often true."
+// );
+
+// export default PlasmoOverlay;
+
+// todo: add logic?
+const CustomButton = () => {
+  return <button className="btn">Custom button</button>;
 };
 
-export const getStyle = () => {
-  const style = document.createElement("style");
-
-  style.textContent = cssText;
-
-  return style;
-};
-
-const PlasmoOverlay = () => {
-  return (
-    <div className="plasmo-z-50 plasmo-flex plasmo-fixed plasmo-top-32 plasmo-right-8"></div>
-  );
-};
-
-export default PlasmoOverlay;
+export default CustomButton;
