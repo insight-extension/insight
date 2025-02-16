f0dbcfdad56cadf3752a02147bb9e9cbba52bf58
Bohdan <76702178+bohdan-mykhailenko@users.noreply.github.com>
2025-02-16 20:04:18 +0200

    chore: add env

--- apps/web/src/tests/unit/specs/jwt.spec.tsx
+++ apps/web/src/tests/unit/specs/jwt.spec.tsx
@@ -4,13 +4,12 @@ import { createJWTTokenWithPayloadAndExpiration } from "../utils/jwt";
 
 describe("JWT Token Comparison", () => {
   it("should correctly compare two different JWT tokens", () => {
-    const publicKey = "6ASFUJeR2dJicMVRV39aghNmny2w3FgBzGssaxX91xQA";
+    const publicKey = ""; // add your wallet;
     const payload = { publicKey, iat: 1739727907 };
 
     const token1 = createJWTTokenWithPayloadAndExpiration(
       payload,
-      // import.meta.env.VITE_JWT_SECRET,
-      "FAWRQW212432RFDAF2FSADCVMASD",
+      import.meta.env.VITE_JWT_SECRET,
       30
     );
 
